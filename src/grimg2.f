ab936dc3fa93b9fde8ff06f6b09ced68dc2c60bd
Ed J <mohawk2@users.noreply.github.com>
2024-12-31 21:11:56 +0000

    CED use optimized resampling in GRIMG2

--- src/grimg2.f
+++ src/grimg2.f
@@ -2,7 +2,8 @@ C*GRIMG2 -- image of a 2D data array (pixel-primitive devices)
 C+
       SUBROUTINE GRIMG2 (A, IDIM, JDIM, I1, I2, J1, J2,
      1                   A1, A2, PA, MININD, MAXIND, MODE)
-      INTEGER IDIM, JDIM, I1, I2, J1, J2, MININD, MAXIND, MODE
+      INTEGER IDIM, JDIM, I1, I2, J1, J2, MININD, MAXIND
+      INTEGER MODE
       REAL    A(IDIM,JDIM)
       REAL    A1, A2
       REAL    PA(6)
@@ -10,15 +11,18 @@ C
 C (This routine is called by GRIMG0.)
 C--
 C 7-Sep-1994  New routine [TJP].
+C 19-Aug-2003 Modified to use optimized resampling [CED]
 C-----------------------------------------------------------------------
       INCLUDE 'grpckg1.inc'
-      INTEGER  I,IV,IX,IX1,IX2,IY,IY1,IY2,J, NPIX, LCHR
-      REAL     DEN, AV, SFAC, SFACL
-      REAL     XXAA,XXBB,YYAA,YYBB,XYAA,XYBB,YXAA,YXBB,XYAAIY,YXAAIY
-      REAL     BUFFER(1026)
+      INTEGER  I,IX,IX1,IX2,IY,IY1,IY2,J, NPIX, LCHR
+      INTEGER  II,III,JJ,JJJ,ISV
+      INTEGER  IV
+      REAL     PPA(6),PINV(6), WGT, XY0, YY0, DI, DJ
+      REAL     BUFFER(1026), AV, SFAC, SFACL
       CHARACTER*1 CHR
       INTRINSIC NINT, LOG
       PARAMETER (SFAC=65000.0)
+
 C-----------------------------------------------------------------------
 C
 C Location of current window in device coordinates.
@@ -27,18 +31,11 @@ C
       IX2 = NINT(GRXMAX(GRCIDE))-1
       IY1 = NINT(GRYMIN(GRCIDE))+1
       IY2 = NINT(GRYMAX(GRCIDE))-1
+
 C
-C Transformation from array coordinates to device coordinates.
+C Prepare the PPA and PINV matrices, and ISV
 C
-      DEN = PA(2)*PA(6)-PA(3)*PA(5)
-      XXAA = (-PA(6))*PA(1)/DEN
-      XXBB = PA(6)/DEN
-      XYAA = (-PA(3))*PA(4)/DEN
-      XYBB = PA(3)/DEN
-      YYAA = (-PA(2))*PA(4)/DEN
-      YYBB = PA(2)/DEN
-      YXAA = (-PA(5))*PA(1)/DEN
-      YXBB = PA(5)/DEN
+      CALL GRSVD( PA, PINV, PPA, ISV )
 C
 C Start a new page if necessary.
 C
@@ -49,19 +46,42 @@ C determine which array pixel (I,J) it falls in.
 C
       SFACL = LOG(1.0+SFAC)
       DO 120 IY=IY1,IY2
-          XYAAIY = XXAA-XYAA-XYBB*IY
-          YXAAIY = YYAA+YYBB*IY-YXAA
+          XY0 = PINV(1) + PINV(3)*IY
+          YY0 = PINV(4) + PINV(6)*IY
           NPIX = 0
           BUFFER(2) = IY
           DO 110 IX=IX1,IX2
-            I = NINT(XYAAIY+XXBB*IX)
+            DI = XY0 + PINV(2)*IX
+            I = NINT(DI)
+            DI = DI - I
             IF (I.LT.I1.OR.I.GT.I2) GOTO 110
-            J = NINT(YXAAIY-YXBB*IX)
-            IF (J.LT.J1.OR.J.GT.J2) GOTO 110
+            DJ = YY0 + PINV(5)*IX
+            J = NINT(DJ)
+            DJ = DJ - J
+C Weight is set to be nonzero but negligible.
+            WGT = 1E-9
+            AV = 0
 C
-C           -- determine color index IV of this pixel
+C Calculate the weighted average of the surrounding array elements
 C
-            AV = A(I,J)
+            DO 104 II=-ISV,ISV
+               III = I + II
+               IF(III.LT.I1 .OR. III.GT.I2) GOTO 104
+               DO 102 JJ = -ISV,SV
+                  JJJ = J + JJ
+                  IF(JJJ.LT.J1 .OR. JJJ.GT.J2) GOTO 102
+C Find the distance in device space using the padded Taylor expansion
+                  DX = ABS ( (II-DI) * PPA(2) + (JJ-DJ) * PPA(3) )
+                  DY = ABS ( (II-DI) * PPA(5) + (JJ-DJ) * PPA(6) )
+C Apply a linear rolloff (factor-of-2 comes out in the wash)
+                  IF ((DX.LT.1) .AND. (DY.LT.1)) THEN
+                     ATMP = (1 - DX) * (1-DY)
+                     WGT = WGT + ATMP
+                     AV = AV + ATMP * A(III,JJJ)
+                  ENDIF
+ 102           CONTINUE
+ 104        CONTINUE
+            AV = AV / WGT
             IF (A2.GT.A1) THEN
                 AV = MIN(A2, MAX(A1,AV))
             ELSE
---
+++ src/grsvd.f
@@ -0,0 +1,129 @@
+C*GRSVD -- SVD portion of GRRGB2 and GRIMG2
+C
+C This is a helper routine for GRRGB2 and GRIMG2 -- you feed in
+C a transformation matrix and get back (A) its inverse, (B) a padded
+C version of the original matrix, and (C) an integer that is guaranteed
+C to be larger than the largest padded singular value of the original matrix.
+C
+C The matrices are all six-element heterogeneous matrices.
+C
+C PA is the only input parameter -- the others are all for output.
+C+
+
+      SUBROUTINE GRSVD(PA, PINV, PPA, ISV)
+      REAL PA(6),PINV(6),PPA(6)
+      INTEGER ISV
+
+      REAL   ATMP,BTMP
+      REAL   THETA,SVX,SVY,SVECX,SVECY,SV1,SV2,R2X,R2Y
+      REAL   XY0,YY0
+
+C Transformation from array coordinates to device coordinates.
+C
+C Transformation is:
+C
+C   X = PA(1) + PA(2)*I + PA(3)*J
+C   Y = PA(4) + PA(5)*I + PA(6)*J
+C
+C Invert the matrix, and calculate offset in array coordinates.
+C The XXINV array converts from device coordinates back to array coordinates.
+
+      ATMP = PA(2)*PA(6)-PA(3)*PA(5)
+      PINV(2) =  PA(6)/ATMP
+      PINV(3) = -PA(3)/ATMP
+      PINV(5) = -PA(5)/ATMP
+      PINV(6) =  PA(2)/ATMP
+      PINV(1) =  - (PINV(2) * PA(1)  +  PINV(3) * PA(4))
+      PINV(4) =  - (PINV(5) * PA(1)  +  PINV(6) * PA(4))
+
+C
+C Singular value decompose the 2x2 inverse matrix to find the range of
+C pixels we have to sample on each side.  (Resolve the matrix TR as
+C R2 x S x R1, where R2 and R1 are rotation matrices and S is the
+C singular value diagonal matrix.)
+C
+C The ATMP/BTMP stuff finds the rotation angle of the SVD basis up
+C to a +/- ambiguity.
+C
+
+      ATMP = PINV(3)**2 + PINV(6)**2 -PINV(2)**2 - PINV(5)**2
+      BTMP = PINV(2)*PINV(3) + PINV(5)*PINV(6)
+
+      IF (ATMP .EQ. 0) THEN
+         THETA = 0
+      ELSE
+         THETA = 0.5 * ASIN( BTMP / SQRT( ATMP**2 / 4 + BTMP**2 ))
+      ENDIF
+
+C Now try the four possibilities to find the largest singular value
+
+      SVX = SIN( THETA )
+      SVY = COS( THETA )
+
+      SV = ( PINV(2) * SVX + PINV(3) * SVY) ** 2 +
+     $     ( PINV(5) * SVX + PINV(6) * SVY) ** 2
+      SVECX = SVX
+      SVECY = SVY
+
+      SV1 = (PINV(2) * SVY + PINV(3) * SVX) ** 2 +
+     $      (PINV(5) * SVY + PINV(6) * SVX) ** 2
+      IF( SV1 .GT. SV ) THEN
+        SVECX = SVY
+        SVECY = SVX
+      ENDIF
+
+      SV1 = ( - PINV(2) * SVX + PINV(3) * SVY ) ** 2 +
+     $      ( - PINV(5) * SVX + PINV(6) * SVY ) ** 2
+      IF( SV1 .GT. SV ) THEN
+        SVECX = -SVX
+        SVECY = SVY
+      ENDIF
+
+      SV1 = ( PINV(2) * SVY - PINV(3) * SVX ) ** 2 +
+     $      ( PINV(5) * SVY - PINV(6) * SVY ) ** 2
+      IF( SV1 .GT. SV ) THEN
+        SVECX = SVY
+        SVECY = -SVX
+      ENDIF
+
+      SV1 = SQRT(SV1)
+
+      ATMP = SQRT(SVECX*SVECX + SVECY*SVECY)
+      SVECX = SVECX/ATMP
+      SVECY = SVECY/ATMP
+
+C SVECX now has the vector associated with the largest singular value,
+C and SV1 has the value itself.  SV2 gets the smallest singular value.
+      SV2 = ( PINV(2) * SVECY - PINV(3) * SVECX ) ** 2 +
+     $      ( PINV(5) * SVECY - PINV(6) * SVECX ) ** 2
+      SV2 = SQRT(SV2)
+
+C Now find the second rotation matrix.
+C Only need two components to know the whole thing.
+      R2X = ( PINV(2) * SVECX + PINV(3) * SVECY ) / SV1
+      R2Y = ( PINV(5) * SVECX + PINV(6) * SVECY ) / SV1
+      R2 = sqrt(R2X*R2X + R2Y*R2Y)
+      R2X = R2X/R2
+      R2Y = R2Y/R2
+
+C Finally, pad the singular values and reconstruct a padded PA by
+C multiplying the rotation matrices and the padded singular values.
+      IF(SV1 .LT. 1) THEN
+         SV1 = 1.
+      ENDIF
+
+      IF(SV2 .LT. 1) THEN
+         SV2 = 1.
+      ENDIF
+
+      PPA(1) = PA(1)
+      PPA(2) =  SVECX * R2X / SV1  -  SVECY * R2Y / SV2
+      PPA(3) = -SVECX * R2Y / SV1  -  SVECY * R2X / SV2
+      PPA(4) = PA(4)
+      PPA(5) =  SVECY * R2X / SV1  +  SVECX * R2Y / SV2
+      PPA(6) = -SVECY * R2Y / SV1  +  SVECX * R2X / SV2
+
+C ISV is the range to average over in array space.
+      ISV = INT(SV2+SV1 + 1)
+
+      END
